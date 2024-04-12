b59f9f40715595012b8156836b6b696cc065face
Tallyson Freitas <tallyson.eu@outlook.com>
2024-04-12 16:32:04 -0300

    feat: adiciona nova linguagem

--- src/components/LinguagemIcon/style.ts
+++ src/components/LinguagemIcon/style.ts
@@ -1,6 +1,17 @@
 import styled from 'styled-components'
 
 export const ContainerLinguagemItem = styled.div`
+  display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
+
+  svg {
+    width: 80px;
+    height: 100%;
+    path {
+      width: 100%;
+      height: 100%;
+    }
+  }
 `
