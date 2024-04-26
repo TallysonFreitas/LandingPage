7be4d82a2b9b3cae20a5fa32369c7ccc3b8ceb8c
Tallyson Freitas <tallyson.eu@outlook.com>
2024-04-26 11:59:55 -0300

    feat: cria estilo para sections

--- src/themeProvider.ts
+++ src/themeProvider.ts
@@ -8,4 +8,11 @@ export const GlobalStyle = createGlobalStyle`
   font-family: 'Roboto', sans-serif;
 }
 
+section{
+  padding:200px 0 160px;
+
+  @media(max-width:768px){
+    padding:120px 0 80px;
+  }
+}
 `
