ada26e031a619b31ff764ebf39a782f259c4da41
Tallyson Freitas <tallyson.eu@outlook.com>
2024-04-26 15:30:33 -0300

    style: ajusta blur

--- src/containers/Header/style.ts
+++ src/containers/Header/style.ts
@@ -2,4 +2,9 @@ import styled from 'styled-components'
 
 export const Container = styled.header`
   backdrop-filter: blur(16px);
+  -webkit-filter: blur(10px); /* Webkit + Blink (Chrome, Safari, etc) */
+  -webkit-filter: blur(10px); /* Webkit + Blink (Chrome, Safari, etc) */
+  -ms-filter: 'progid:DXImageTransform.Microsoft.Blur(PixelRadius=10)'; /* IE 8 */
+  filter: progid:DXImageTransform.Microsoft.Blur(PixelRadius='10'); /* IE 7 < */
+  filter: url(blur-filter.svg#10); /* Firefox */
 `
