fa10f1469c846162a69edc281ffce96e066ea1ad
Tallyson Freitas <tallyson.eu@outlook.com>
2024-04-26 12:08:58 -0300

    fix: corrige filtro invalido

--- src/containers/Header/style.ts
+++ src/containers/Header/style.ts
@@ -3,5 +3,4 @@ import styled from 'styled-components'
 export const Container = styled.header`
   background-color: transparent;
   backdrop-filter: blur(16px);
-  -webki-backdrop-filter: blur(16px);
 `
