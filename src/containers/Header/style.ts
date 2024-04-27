9d98cad4e347df9b4dd4ab6e7e0b17883117d123
Tallyson Freitas <tallyson.eu@outlook.com>
2024-04-27 06:57:15 -0300

    style: da cor ao Header

--- src/containers/Header/style.ts
+++ src/containers/Header/style.ts
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
 
-export const Container = styled.header`
+export const Container = styled.header`\
+background-color:rgba(160,160,160, 0.1);
+
   backdrop-filter: blur(16px);
   -webkit-filter: blur(10px); /* Webkit + Blink (Chrome, Safari, etc) */
   -webkit-filter: blur(10px); /* Webkit + Blink (Chrome, Safari, etc) */
