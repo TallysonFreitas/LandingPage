e373523058755d315dfe24c36048e64f333e6f67
Tallyson Freitas <tallyson.eu@outlook.com>
2024-04-29 07:04:44 -0300

    feat: cria estilo Title

---
+++ src/containers/Contact/style.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Title = styled.h2` font-family: 'Bree Serif', serif;
+font-weight: 400;
+font-style: normal;
+font-size: 3em;
+
+span {
+  font-family: 'Bree Serif', serif;
+`
