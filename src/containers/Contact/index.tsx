54ec791d6844ad52667d22bd81e7955cfd907b10
Tallyson Freitas <tallyson.eu@outlook.com>
2024-04-29 07:05:00 -0300

    feat: adiciona componente etiliizado Title

--- src/containers/Contact/index.tsx
+++ src/containers/Contact/index.tsx
@@ -1,15 +1,17 @@
 import FormBootstrap from '../../components/FormBootstrap'
 
+import * as S from './style'
+
 const Contact = () => {
   return (
     <section className="bg-dark text-white" id="contact">
       <div className="text-center mb-5">
-        <h2 className="fs-1 fw-bold">
-          Send me a <span className="text-danger">Email</span>.
-        </h2>
-        <h2 className="fs-1 fw-bold">
-          I&apos;m ready to <span className="text-danger">Work</span>.
-        </h2>
+        <S.Title className="fs-1 fw-bold text-whited">
+          Send me a <span className="text-danger">Email</span>
+        </S.Title>
+        <S.Title className="fs-1 fw-bold text-whited">
+          I&apos;m ready to <span className="text-danger">Work</span>
+        </S.Title>
       </div>
       <div className="container">
         <div className="row d-flex justify-content-center">
